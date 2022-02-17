d84cd470e454d47025c611d1f06de6905bfc2787
Mohammed Awais <98485935+mo-awais@users.noreply.github.com>
2022-02-17 10:16:05 +0000

    Update port_scanner.py
    I have listed below, the PEP 8 changes, some efficiency changes and then actual bug fixes:
    
    **General Improvements**
        - The docstring description at the top must use double quotes not single.
        - You have imported subprocess, but you don't use it. This should be removed.
        - Your indentations are 2 spaces. Python PEP conventions requires indent spaces of multiples of 4. You should use indentations of 4 spaces. **(PEP 8: E111)**
        - All Python files should have a blank newline at the end of the file. **(PEP 8: W292)**
        - At line 21, the `range(1, 5000)` has a whitespace between `range` and the brackets `(1, 5000)`. You should not use a whitespace here. **(PEP 8: E2111)**
        - Changes `format(port)` to f-string literals.
    
    **Bug Fixes**
        - Add a timeout to the socket to prevent the socket from waiting when trying closed ports (I have set this to 1 second, you can adjust this to your timing).
        - Add a print statement to output the status of a closed port, this will show progress of the scan.

--- port_scanner.py
+++ port_scanner.py
@@ -1,12 +1,12 @@
-'''
+"""
 Port Scanning App
 From Infosec's Youtube video posted on 27Aug21
 https://www.youtube.com/watch?v=t9EX2RAUoTU
-'''
+"""
 
 import socket
-import subprocess
 import sys
+import timeit
 from datetime import datetime
 
 remoteServer = input("Enter a remote host to scan: ")
@@ -18,22 +18,26 @@ print("_" * 60)
 t1 = datetime.now()
 
 try:
-  for port in range (1, 5000):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((remoteServerIP, port))
-    if result == 0:
-      print("Port {}:         Open".format(port))
-    sock.close()
+    for port in range(1, 5000):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0:
+            print(f"Port {port}:         Open")
+        else:
+            print(f"Port {port}:         Closed")
+            end = timeit.timeit()
+        sock.close()
 
 except KeyboardInterrupt:
-  print("Hostname could not be resolved. Exiting")
-  sys.exit()
+    print("Hostname could not be resolved. Exiting")
+    sys.exit()
 except socket.gaierror:
-  print("Hostname could not be resolved. Exiting")
-  sys.exit()
+    print("Hostname could not be resolved. Exiting")
+    sys.exit()
 except socket.error:
-  print("Couldn't connect to server")
-  sys.exit()
+    print("Couldn't connect to server")
+    sys.exit()
 
 t2 = datetime.now()
 
