b51bd27585b5f57734a83aef8a9d4f7a1355134a
Leericly <95377567+Leericly@users.noreply.github.com>
2022-02-20 09:39:23 -0800

    commit

---
+++ python_lists.py
@@ -0,0 +1,9 @@
+# More code to follow but I need to go back and do some exercises with lists and tuples.
+
+# LISTS
+
+mylist = []             # This creates a basic empty list. A a type of Python data structure.
+mylist.append(1)        # .append() adds a single value to the list.
+
+mylist2 = [1, 2, 3, "text"]
+print(mylist2)
